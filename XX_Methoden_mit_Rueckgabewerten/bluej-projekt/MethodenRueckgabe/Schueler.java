89e08f05348dfbb6e5ee81bbc83f21decdbd500d
C. Krichenbauer <schule@krichenbauer.de>
2019-10-10 17:02:37 +0200

    initial commit

---
+++ XX_Methoden_mit_Rueckgabewerten/bluej-projekt/MethodenRueckgabe/Schueler.java
@@ -0,0 +1,15 @@
+class Schueler
+{
+    int geburtsjahr;
+    String vorname;
+    String nachname;
+    boolean maennlich;
+    
+    Schueler(int gj, String vn, String nn, boolean m) {
+        geburtsjahr = gj;
+        vorname = vn;
+        nachname = nn;
+        maennlich = m;
+    }
+    
+}
